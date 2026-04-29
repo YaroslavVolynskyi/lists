77e45b7fd8d2f2457677822c8192a32a68c592e0
Yaroslav <yarik93v@gmail.com>
2026-04-29 11:25:45 -0700

    save cursor position

--- app/src/main/java/com/fin/shoppinglist123/ui/ShoppingList.kt
+++ app/src/main/java/com/fin/shoppinglist123/ui/ShoppingList.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -93,7 +94,7 @@ fun ShoppingListScreen(
                 Card(modifier = Modifier.fillMaxWidth()) {
                     if (isEditing) {
                         val focusRequester = remember { FocusRequester() }
-                        var textFieldValue by remember {
+                        var textFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) {
                             mutableStateOf(
                                 TextFieldValue(
                                     text = uiState.editingText,
